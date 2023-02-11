c894ea44630587a52deca36dc8e1e417d7160a71
CALVINCE <calvince.ogwel@gmail.com>
2023-02-11 16:57:02 +0300

    completed object inheritance code along

--- lib/car.rb
+++ lib/car.rb
@@ -1,5 +1,5 @@
 # This loads the code from the vehicle.rb file so we can access its code within this file
-# require_relative './vehicle'
+require_relative './vehicle'
 
 class Car < Vehicle
     def go
